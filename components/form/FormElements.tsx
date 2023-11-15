d91b112c843c26b0d59bf7607944059835ba3e54
nonsodaniel <nonsodaniel07@gmail.com>
2023-11-15 17:38:37 +0100

    Added text area field

--- components/form/FormElements.tsx
+++ components/form/FormElements.tsx
@@ -3,6 +3,7 @@ import { ParagprahFieldFormElement } from "@/fields/ParagraphField";
 import { SeparatorFieldFormElement } from "@/fields/SeparatorField";
 import { SpacerFieldFormElement } from "@/fields/SpacerField";
 import { SubTitleFieldFormElement } from "@/fields/SubTitleField";
+import { TextAreaFormElement } from "@/fields/TextAreaField";
 import { TextFieldFormElement } from "@/fields/TextField";
 import { TitleFieldFormElement } from "@/fields/TitleField";
 
@@ -13,7 +14,8 @@ export type ElementsType =
   | "ParagraphField"
   | "SeparatorField"
   | "SpacerField"
-  | "NumberField";
+  | "NumberField"
+  | "TextAreaField";
 
 export type SubmitFunction = (key: string, value: string) => void;
 
@@ -60,4 +62,5 @@ export const FormElements: FormElementsType = {
   SeparatorField: SeparatorFieldFormElement,
   SpacerField: SpacerFieldFormElement,
   NumberField: NumberFieldFormElement,
+  TextAreaField: TextAreaFormElement,
 };
