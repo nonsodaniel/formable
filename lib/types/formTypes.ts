d32669cda5f86359e4e67fa380d10faf35ff0c82
nonsodaniel <nonsodaniel07@gmail.com>
2023-11-15 20:54:13 +0100

    Fix merge conflicts

--- lib/types/formTypes.ts
+++ lib/types/formTypes.ts
@@ -1,6 +1,27 @@
 import { TextFieldFormElement } from "@/fields/TextField";
+import { CheckboxFieldFormElement } from "@/fields/CheckboxField";
+import { DateFieldFormElement } from "@/fields/DateField";
+import { NumberFieldFormElement } from "@/fields/NumberField";
+import { ParagprahFieldFormElement } from "@/fields/ParagraphField";
+import { SelectFieldFormElement } from "@/fields/SelectField";
+import { SeparatorFieldFormElement } from "@/fields/SeparatorField";
+import { SpacerFieldFormElement } from "@/fields/SpacerField";
+import { SubTitleFieldFormElement } from "@/fields/SubTitleField";
+import { TextAreaFormElement } from "@/fields/TextAreaField";
+import { TitleFieldFormElement } from "@/fields/TitleField";
 
-export type ElementsType = "TextField";
+export type ElementsType =
+  | "TextField"
+  | "TitleField"
+  | "SubTitleField"
+  | "ParagraphField"
+  | "SeparatorField"
+  | "SpacerField"
+  | "NumberField"
+  | "TextAreaField"
+  | "DateField"
+  | "SelectField"
+  | "CheckboxField";
 
 export type SubmitFunction = (key: string, value: string) => void;
 
@@ -41,4 +62,14 @@ type FormElementsType = {
 };
 export const FormElements: FormElementsType = {
   TextField: TextFieldFormElement,
+  TitleField: TitleFieldFormElement,
+  SubTitleField: SubTitleFieldFormElement,
+  ParagraphField: ParagprahFieldFormElement,
+  SeparatorField: SeparatorFieldFormElement,
+  SpacerField: SpacerFieldFormElement,
+  NumberField: NumberFieldFormElement,
+  TextAreaField: TextAreaFormElement,
+  DateField: DateFieldFormElement,
+  SelectField: SelectFieldFormElement,
+  CheckboxField: CheckboxFieldFormElement,
 };
